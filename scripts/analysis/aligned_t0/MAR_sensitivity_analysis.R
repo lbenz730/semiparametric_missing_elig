20dd3a5b78397cbe9e28af4106620b81054db483
lbenz730 <lukesbenz@gmail.com>
2026-09-23 22:27:16 -0400

    Update sensitivity analysis

--- scripts/analysis/aligned_t0/MAR_sensitivity_analysis.R
+++ scripts/analysis/aligned_t0/MAR_sensitivity_analysis.R
@@ -25,21 +25,61 @@ t2dm_EIF <- map_dfr(t2dm_files, read_parquet)
 ### Sensitivity analysis for collection of EIF contributions given
 ### sensitivity params alpha_a1c, alpha_bmi which correspond to the 
 ### log odds in being complete case (R = 1) per unit increase in a1c or bmi
-sensitivity <- function(df_EIF, alpha_a1c, alpha_bmi) {
+sensitivity <- function(df_EIF, outcome_var, alpha_a1c, alpha_bmi, n_splits = 2) {
   
-  df_final <-
+  df_EIF$outcome <- df_EIF[[outcome_var]]
+  
+  ### Exponential Sensitivity Tilt
+  df_EIF <- 
     df_EIF %>% 
+    mutate('h' = case_when(R == 0 ~ 0,
+                           R == 1 ~ alpha_bmi * ifelse(!is.na(baseline_bmi), (baseline_bmi - 35), 0) + 
+                             alpha_a1c * ifelse(!is.na(baseline_a1c), (baseline_a1c - 6.5), 0)),
+           'exp_minus_h' = exp(-h),
+           'kappa_0' = NA,
+           'kappa_1' = NA)
+  
+  
+  
+  ### Sample Split to estimate kappa, seperately for each lookback scenario
+  for(scenario in unique(df_EIF$scenario_id)) {
+    df_scenario <- 
+      df_EIF %>% 
+      filter(scenario_id == scenario)
     
+    split_ids <- 
+      split(sample( 1:nrow(df_scenario) ), ceiling(1:nrow(df_scenario) / ceiling(nrow(df_scenario)/n_splits)))
+    
+    for(i in 1:n_splits) {
+      test_ids <- split_ids[[i]]
+      train_ids <- setdiff(1:nrow(df_scenario), test_ids)
+      df_train <- dplyr::slice(df_scenario, train_ids)
+      df_test <- dplyr::slice(df_scenario, test_ids)
+      
+      kappa <- 
+        glm(exp_minus_h ~ 
+              site + gender + baseline_age + smoking_status + eGFR + 
+              race + hypertension + dyslipidemia + calendar_year + A + 
+              A * baseline_age + A * gender,
+            family = quasipoisson(link = 'log'),
+            data = df_train %>% filter(R == 1))
+      
+      df_scenario$kappa_0[test_ids] <- predict(kappa, newdata = df_test %>% mutate('A' = 0), type = 'response')
+      df_scenario$kappa_1[test_ids] <- predict(kappa, newdata = df_test %>% mutate('A' = 1), type = 'response')
+    }
+    df_EIF$kappa_0[df_EIF$scenario_id == scenario] <- df_scenario$kappa_0
+    df_EIF$kappa_1[df_EIF$scenario_id == scenario] <- df_scenario$kappa_1
+  }
+  
+  
+  df_final <- 
+    df_EIF %>% 
     ### Update eta w/ sensitivity parameters
-    mutate('eta1_hat' =  case_when(R == 0 ~ eta1_hat,
-                                   R == 1 ~ expit(logit(eta1_hat) + 
-                                                    alpha_bmi * ifelse(!is.na(baseline_bmi), (baseline_bmi - 35), 0) + 
-                                                    alpha_a1c * ifelse(!is.na(baseline_a1c), (baseline_a1c - 6.5), 0))),
+    mutate('eta1_tilde' =  case_when(R == 0 ~ eta1_hat,
+                                     R == 1 ~ 1/(1 + (1-eta1_hat)/eta1_hat * exp_minus_h/kappa_1)),
            
-           'eta0_hat' =  case_when(R == 0 ~ eta0_hat,
-                                   R == 1 ~ expit(logit(eta0_hat) +  
-                                                    alpha_bmi * ifelse(!is.na(baseline_bmi), (baseline_bmi - 35), 0) + 
-                                                    alpha_a1c * ifelse(!is.na(baseline_a1c), (baseline_a1c - 6.5), 0)))) %>% 
+           'eta0_tilde' =  case_when(R == 0 ~ eta0_hat,
+                                     R == 1 ~ 1/(1 + (1-eta0_hat)/eta0_hat * exp_minus_h/kappa_0))) %>% 
     
     
     ### need some value to get canceled by 0 that's not NA
@@ -50,24 +90,25 @@ sensitivity <- function(df_EIF, alpha_a1c, alpha_bmi) {
            'u_hat' = replace(u_hat, R == 1 & is.na(u_hat), 0)
     ) %>%
     ### Compute numerator and denominator one step estimators (eg., uncentered influence functions)
-    mutate('alpha_OS' = A * (1 - R/eta1_hat) * epsilon1_hat + A * R * eligible/eta1_hat,
+    mutate('alpha_OS' = A * (1 - R/eta1_tilde) * epsilon1_hat + A * R * eligible/eta1_tilde,
            'beta_OS' = 
-             A * (epsilon1_hat * pct_wt_change - xi_hat)
-           + A * R/eta1_hat * ((eligible - epsilon1_hat) * pct_wt_change - (eligible * mu0_hat - xi_hat))
-           - (1-A) * R/eta1_hat * (eligible * u_hat/(1-u_hat) * (pct_wt_change - mu0_hat) - (gamma_hat * pct_wt_change - chi_hat))
-           - (1-A) * eta0_hat/eta1_hat * ((gamma_hat * pct_wt_change - chi_hat))
+             A * (epsilon1_hat * outcome - xi_hat)
+           + A * R/eta1_tilde * ((eligible - epsilon1_hat) * outcome - (eligible * mu0_hat - xi_hat))
+           - (1-A) * R/eta1_tilde * (eligible * u_hat/(1-u_hat) * (outcome - mu0_hat))
+           + (1-A) * eta0_hat/eta1_hat * (R/eta0_tilde - 1) * ((gamma_hat * outcome - chi_hat))
     ) %>% 
     group_by(scenario_id) %>% 
     ### Subject Specific Contributions (centered)
     mutate('alpha_IF' = alpha_OS - mean(alpha_OS),
-           'beta_IF' = beta_OS - mean(beta_OS))
+           'beta_IF' = beta_OS - mean(beta_OS)) %>% 
+    mutate('subject_IF' = (beta_OS - mean(beta_OS)/mean(alpha_OS) * alpha_OS)/mean(alpha_OS)) 
   
   
   tau_hat <-
     df_final %>% 
     group_by(scenario_id, diabetes_lookback, bmi_lookback, rx_lookback) %>% 
     summarise('att_hat' = mean(beta_OS)/mean(alpha_OS),
-              'sd' = sd(subject_IF)/sqrt(nrow(df_EIF)),
+              'sd' = sd(subject_IF)/sqrt(n()),
               'alpha_a1c' = alpha_a1c,
               'alpha_bmi' = alpha_bmi) %>% 
     ungroup()
@@ -75,18 +116,22 @@ sensitivity <- function(df_EIF, alpha_a1c, alpha_bmi) {
   return(tau_hat)
 }
 
-
+### Alpha Grid
 alpha_grid <- 
   crossing('alpha_a1c' = seq(-0.3, 0.3, 0.03),
-           'alpha_bmi' = seq(-0.125, 0.125, 0.01))
+           'alpha_bmi' = seq(-0.15, 0.15, 0.01))
 
 df_weight <- 
-  future_map2_dfr(alpha_grid$alpha_a1c, alpha_grid$alpha_bmi, ~sensitivity(weight_EIF, .x, .y))
+  future_map2_dfr(alpha_grid$alpha_a1c, alpha_grid$alpha_bmi, ~sensitivity(weight_EIF, 'pct_wt_change', .x, .y),
+                  .options = furrr_options(seed = 158), 
+                  .progress = T)
 
 
 
 df_t2dm <- 
-  future_map2_dfr(alpha_grid$alpha_a1c, alpha_grid$alpha_bmi, ~sensitivity(t2dm_EIF, .x, .y))
+  future_map2_dfr(alpha_grid$alpha_a1c,  alpha_grid$alpha_bmi, ~sensitivity(t2dm_EIF, 'remission', .x, .y),
+                  .options = furrr_options(seed = 150),
+                  .progress = T)
 
 write_csv(df_weight, glue('{data_dir}/aligned_t0/outputs/weight_change_sensitivity.csv'))
 write_csv(df_t2dm, glue('{data_dir}/aligned_t0/outputs/t2dm_sensitivity.csv'))
@@ -107,8 +152,8 @@ weight_plot <-
 ggplot(weight_plot %>% filter(rx_lookback == 0), aes(x = alpha_a1c, y = alpha_bmi)) + 
   facet_grid(bmi_lookback_f~a1c_lookback_f, labeller = label_parsed) + 
   geom_tile(aes(fill = att_hat), color = 'black', lwd = 0.05) +
-  scale_fill_gradient2(low = 'red', mid = 'white', high = 'blue') +
-  scale_x_continuous(breaks = seq(-0.3, 0.3, 0.15)) + 
+  scale_fill_gradient2(low = 'red', mid = 'white', high = 'blue', na.value = 'lightgrey') +
+  scale_x_continuous(breaks = seq(-0.6, 0.6, 0.15)) + 
   scale_y_continuous(breaks = c(-0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15)) + 
   theme(legend.text = element_text(angle = 90, hjust = 1),
         axis.title = element_text(size = 18),
@@ -127,7 +172,7 @@ ggsave('figures/aligned_t0/application/weight_sensitivity_0.pdf', height = 12/1.
 ggplot(weight_plot %>% filter(rx_lookback == 12), aes(x = alpha_a1c, y = alpha_bmi)) + 
   facet_grid(bmi_lookback_f~a1c_lookback_f, labeller = label_parsed) + 
   geom_tile(aes(fill = att_hat), color = 'black', lwd = 0.05) +
-  scale_fill_gradient2(low = 'red', mid = 'white', high = 'blue') +
+  scale_fill_gradient2(low = 'red', mid = 'white', high = 'blue', na.value = 'lightgrey') +
   scale_x_continuous(breaks = seq(-0.3, 0.3, 0.15)) + 
   scale_y_continuous(breaks = c(-0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15)) + 
   theme(legend.text = element_text(angle = 90, hjust = 1),
@@ -143,12 +188,12 @@ ggplot(weight_plot %>% filter(rx_lookback == 12), aes(x = alpha_a1c, y = alpha_b
        fill = expression(paste(widehat(theta)[ATT]^elig, ' = ', widehat(E), '[', Y^RYGB - Y^SG, ' | A = RYGB, Eligible = 1]')))
 
 ggsave('figures/aligned_t0/application/weight_sensitivity_12.pdf', height = 12/1.2, width = 16/1.2)
-       
+
 
 
 t2dm_plot <- 
   df_t2dm %>% 
-  filter(alpha_bmi >= -0.05) %>% 
+  filter(abs(alpha_bmi) <= 0.125) %>% 
   mutate('bmi_lookback_f' = paste0('BMI~Lookback:~', bmi_lookback, ifelse(bmi_lookback > 1, '~Months', '~Month')),
          'a1c_lookback_f' = paste0('A1c~Lookback:~', diabetes_lookback, ifelse(diabetes_lookback > 1, '~Months', '~Month')),
          'rx_lookback_f' = ifelse(rx_lookback == '0', '"Diabetes via Rx:\nActive at Surgery"', 
@@ -161,7 +206,7 @@ t2dm_plot <-
 ggplot(t2dm_plot %>% filter(rx_lookback == 0), aes(x = alpha_a1c, y = alpha_bmi)) + 
   facet_grid(bmi_lookback_f~a1c_lookback_f, labeller = label_parsed) + 
   geom_tile(aes(fill = att_hat), color = 'black', lwd = 0.05) +
-  scale_fill_gradient2(low = 'red', mid = 'white', high = 'blue') +
+  scale_fill_gradient2(high = 'red', mid = 'white', low = 'blue') +
   scale_x_continuous(breaks = seq(-0.3, 0.3, 0.15)) + 
   scale_y_continuous(breaks = c(-0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15)) + 
   theme(legend.text = element_text(angle = 90, hjust = 1),
@@ -181,7 +226,7 @@ ggsave('figures/aligned_t0/application/t2dm_sensitivity_0.pdf', height = 12/1.2,
 ggplot(t2dm_plot %>% filter(rx_lookback == 12), aes(x = alpha_a1c, y = alpha_bmi)) + 
   facet_grid(bmi_lookback_f~a1c_lookback_f, labeller = label_parsed) + 
   geom_tile(aes(fill = att_hat), color = 'black', lwd = 0.05) +
-  scale_fill_gradient2(low = 'red', mid = 'white', high = 'blue') +
+  scale_fill_gradient2(high = 'red', mid = 'white', low = 'blue') +
   scale_x_continuous(breaks = seq(-0.3, 0.3, 0.15)) + 
   scale_y_continuous(breaks = c(-0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15)) + 
   theme(legend.text = element_text(angle = 90, hjust = 1),
